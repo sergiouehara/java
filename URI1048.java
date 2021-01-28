2d4093809b08508fa56ca53323be309a081fd888
sergiouehara <s.hiroshi.uehara@uol.com.br>
2021-01-28 09:51:30 -0300

    masi arquivos java

--- URI1048.java
+++ URI1048.java
@@ -3,23 +3,31 @@ public class URI1048{
 
     public static void main(String args[]){
         Scanner teclado = new Scanner(System.in);
-        double salario, novosal, reajuste, percentual ; 
-        
-        salario = teclado.nextDouble(); 
+
+        float salario, novosal, reajuste; 
+        int percentual;
+
+        salario = teclado.nextFloat(); 
                 
-        if (salario >= 0 && salario <= 400){
-           novosal = salario * 1.15;   
-           reajuste = salario * 0.15; 
-           System.out.println("Novo salario =" + novosal);
-           System.out.println("Reajuste = %.2f\n", reajuste);  
-           System.out.println("Em percentual = 15%"); 
+        if (salario >= 0 && salario <= 400){             
+           percentual = 15; 
         }
-        else if (salario > 400){
-           System.out.println("Eixo Y"); 
+        else if (salario <= 800){
+           percentual =  12;
         }
-        else if (salario > 800){
-           System.out.println("Eixo X"); 
-        }        
-        else System.out.println("Q3"); 
+        else if (salario <= 1200){
+           percentual = 10;  
+        }       
+        else if (salario <= 2000){
+           percentual = 7;  
+        }       
+        else percentual = 4;
+
+        reajuste = salario*percentual/100;     
+        novosal =  salario + reajuste; 
+        System.out.printf("Novo salario: %.2f\n", novosal);
+        System.out.printf("Reajuste ganho: %.2f\n", reajuste);  
+        System.out.println("Em percentual: " +percentual " %" ); 
     }
 } 
+           
